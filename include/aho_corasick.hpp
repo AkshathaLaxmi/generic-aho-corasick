0a1f921cdab8ca5eaeff5e465a96fce69b8becfe
temanisparsh <sparshtemani31415@gmail.com>
2021-05-05 18:46:25 +0530

    Add comments

--- include/aho_corasick.hpp
+++ include/aho_corasick.hpp
@@ -9,12 +9,12 @@
 template <typename T, typename V>
 class AhoCorasick
 {
-    vector<Node<V>> trie;
-    vector<T> patterns;
+    vector<Node<V>> trie; // Contains the trie in the form of a vecto where each Node hold a value of type V and has children and parent indices
+    vector<T> patterns;   // Contains the list of the patterns that are supposed to be matched
 
-    void BuildTrie();
-    int getFallback(int currentNode);
-    int getTransition(int currentNode, V value);
+    void BuildTrie();                            // Method to build the trie using the patterns ( preprocessing stage )
+    int getFallback(int currentNode);            // Method to get the fallback transition to the longest suffix if pattern match fails
+    int getTransition(int currentNode, V value); // Method to get the transition from current state to next element in the sequence
 
 public:
     map<typename T::iterator, vector<T>> MatchPattern(T &sequence);
@@ -24,23 +24,28 @@ public:
 template <typename T, typename V>
 void AhoCorasick<T, V>::BuildTrie()
 {
-    Node<V> root = Node<V>();
-    this->trie.push_back(root);
+    Node<V> root = Node<V>();   // Initializes the root node
+    this->trie.push_back(root); // Pushed the root node into the empty trie
 
     int patternSize = this->patterns.size();
+    // Iterates over each pattern
     for (int patternIndex = 0; patternIndex < patternSize; ++patternIndex)
     {
         T pattern = this->patterns[patternIndex];
         int currentNode = 0;
+        // Iterates over each element in the pattern
         for (V value : pattern)
         {
+            // If node doesn't exist in map, adds new node to trie
             if (this->trie[currentNode].next.find(value) == this->trie[currentNode].next.end())
             {
                 this->trie[currentNode].next[value] = this->trie.size();
                 this->trie.emplace_back(value, currentNode);
             }
+            // Transitions to the next node corresponding the currentValue
             currentNode = this->trie[currentNode].next[value];
         }
+        // Marks the last node of the pattern and stores the pattern index to determine pattern size later on
         this->trie[currentNode].patternIndex = patternIndex;
     }
 }
@@ -48,51 +53,67 @@ void AhoCorasick<T, V>::BuildTrie()
 template <typename T, typename V>
 int AhoCorasick<T, V>::getFallback(int currentNode)
 {
+    // Check is value id cached, is not, updates cahe
     if (this->trie[currentNode].link == -1)
     {
+        // If the node is root node or an immediate child of rootnode, fallback node is always root node
+        // This is because the length of the longest suffix exclusive of the current state is 0
         if (currentNode == 0 || this->trie[currentNode].parent == 0)
         {
             this->trie[currentNode].link = 0;
         }
+        // Else gets the fallback node of the parent and tries to transition to the current values
+        // Recursive process till the fallback node becomes root or the longest suffix is found
         else
         {
             this->trie[currentNode].link = this->getTransition(this->getFallback(this->trie[currentNode].parent), this->trie[currentNode].value);
         }
     }
+    // Returns cached value
     return this->trie[currentNode].link;
 }
 
 template <typename T, typename V>
 int AhoCorasick<T, V>::getTransition(int currentNode, V value)
 {
+    // Check is value id cached, is not, updates cahe
     if (this->trie[currentNode].cached.find(value) == this->trie[currentNode].cached.end())
     {
+        // Checks if normal transition exists, if yes...transitions to this node
         if (this->trie[currentNode].next.find(value) != this->trie[currentNode].next.end())
         {
             this->trie[currentNode].cached[value] = this->trie[currentNode].next[value];
         }
+        // else, if current node is root node, can transition directly to root node
         else if (currentNode == 0)
         {
             trie[currentNode].cached[value] = 0;
         }
+        // or else get the fallback node of the current node and tries to transition to "value"
+        // If transition doesn't exist, recusrsively gets fallback till transition is possible or reaches root node
         else
         {
             trie[currentNode].cached[value] = this->getTransition(this->getFallback(currentNode), value);
         }
     }
+    // Returns cached value
     return this->trie[currentNode].cached[value];
 }
 
 template <typename T, typename V>
 map<typename T::iterator, vector<T>> AhoCorasick<T, V>::MatchPattern(T &sequence)
 {
+    // Stores result as a map of iterator:vector<Pattern Type>
+    // As multiple patterns can start at same positon
     map<typename T::iterator, vector<T>> result;
 
+    // Starts matching at root node
     int currentNode = 0;
     typename T::iterator sequenceBegin = sequence.begin();
     typename T::iterator sequenceEnd = sequence.end();
 
     vector<typename T::iterator> iterators;
+    // Stores iterators of all positions for future use
     for (typename T::iterator it = sequenceBegin; it != sequenceEnd; ++it)
     {
         iterators.push_back(it);
@@ -100,27 +121,33 @@ map<typename T::iterator, vector<T>> AhoCorasick<T, V>::MatchPattern(T &sequence
 
     int iteratorsLength = iterators.size();
 
+    // Iterates through the sequence to be searched
     for (int iteratorIndex = 0; iteratorIndex < iteratorsLength; ++iteratorIndex)
     {
         typename T::iterator it = iterators[iteratorIndex];
         V value = *it;
+        // Performs transitions with the alue at the index in the sequence
         currentNode = this->getTransition(currentNode, value);
+        // If there is a pattern match, ie. index != -1, adds it to the result
         if (this->trie[currentNode].patternIndex != -1)
         {
             int patternIndex = this->trie[currentNode].patternIndex;
             int matchedPatternSize = this->patterns[patternIndex].size();
             typename T::iterator patternStart = iterators[iteratorIndex - matchedPatternSize + 1];
+            // Checks if the iterator entry already exists, if not..adds one
             if (result.find(patternStart) == result.end())
             {
                 vector<T> temp;
                 result[patternStart] = temp;
             }
+            // Adds the pattern that matched to the map
             result[patternStart].push_back(this->patterns[patternIndex]);
         }
     }
     return result;
 }
 
+// Stores the patterns and starts the preprocessing
 template <typename T, typename V>
 AhoCorasick<T, V>::AhoCorasick(vector<T> patterns)
 {
--- include/aho_corasick_node.hpp
+++ include/aho_corasick_node.hpp
@@ -4,21 +4,23 @@
 #include <map>
 using namespace std;
 
+// Used to store single node in the trie of the patterns
 template <typename T>
 class Node
 {
 public:
-    T value;
-    int parent;
-    map<T, int> next;
-    map<T, int> cached;
-    int link;
-    int patternIndex;
+    T value;            // Stores the value at the node, null by default
+    int parent;         // Store the index of the parent node of the trie
+    map<T, int> next;   // Store the mapping of value to child nodes
+    map<T, int> cached; // Used to cache 'next' values to avoid recomputation
+    int link;           // Stores the index of the fallback if the transition fails
+    int patternIndex;   // Store the index of the pattern in the pattern vector if it is the last elemtn in the pattern
 
-    Node();
-    Node(T value, int parent = 0);
+    Node();                        // Used by the root node since value is null
+    Node(T value, int parent = 0); // Used by all non root nodes
 };
 
+// Constructors for creating a node in the Trie
 template <typename T>
 Node<T>::Node(T value, int parent)
 {
