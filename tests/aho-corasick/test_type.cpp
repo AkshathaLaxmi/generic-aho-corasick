998c726c64b14769d2ffc48e0cb0b62be1702b1e
temanisparsh <sparshtemani31415@gmail.com>
2021-05-06 17:22:34 +0530

    Update tests

--- tests/aho-corasick/test_type.cpp
+++ tests/aho-corasick/test_type.cpp
@@ -9,6 +9,11 @@ void test_vectors()
     vector<vector<vector<int>>> patterns = {{{4, 5, 2}, {5, 7, 3}, {5, 7, 3}}, {{4, 5, 2}, {100, 44, 56}, {5, 7, 3}}, {{5, 7, 3}, {4, 5, 2}, {100, 44, 56}}, {{2, 3, 4}, {5, 7, 3}, {2, 3, 4}}};
     AhoCorasick<vector<vector<int>>, vector<int>> search = AhoCorasick<vector<vector<int>>, vector<int>>(patterns);
     map<vector<vector<int>>::iterator, vector<vector<vector<int>>>> result = search.MatchPattern(text);
+
+    for (pair<vector<vector<int>>::iterator, vector<vector<vector<int>>>> item : result)
+    {
+        cout << "Iterator start: " << item.first - text.begin() << endl;
+    }
 }
 
 void test_sets()
